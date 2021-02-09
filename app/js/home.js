b8671053e6ed37158b0ff12c9faed680fa87418a
jgustavoj <gjime011@fiu.edu>
2021-02-08 22:00:06 -0500

    views added

--- app/js/home.js
+++ app/js/home.js
@@ -1,1 +1,13 @@
 console.log("Testing home js file");
+
+document.querySelector("#project1").addEventListener("click", () => {
+  window.location.href = "/app/views/project1.html";
+});
+
+document.querySelector("#project2").addEventListener("click", () => {
+  window.location.href = "/app/views/project1.html";
+});
+
+document.querySelector("#project3").addEventListener("click", () => {
+  window.location.href = "/app/views/project1.html";
+});
